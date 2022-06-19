93e5fc29f9d3f226ae0bb3ae5869e87f6fa43af7
Ivan Bovkush <ivan.bovkush@gmail.com>
2022-06-19 17:28:58 +0300

    fix: some errors

--- front/src/components/ui/card/Cardgm.vue
+++ front/src/components/ui/card/Cardgm.vue
@@ -9,12 +9,13 @@
     components: {
     
     },
+
     data() {
       return {
         api: import.meta.env.VITE_API,
         userId: JSON.parse(localStorage.getItem('headers')).userId,
         token: JSON.parse(localStorage.getItem('headers')).token,
-        user: {},
+        user: {}
       }
     },
     methods: {
@@ -54,59 +55,96 @@
       },
       modifyOnePost: function() {
        if(confirm("Voulez-vous vraiment modifier votre post?")){
-        fetch(`${this.api}/api/post/${this.postData._id}`, {
-            method: 'PUT',
-            headers: {
-              'authorization': this.token,
-              'Content-Type': 'application/json'
-            },
-          })
-          .then(response => response.json())
-          .then(data => {
-            if(data.error) {
-              console.log(error)
-            } else {
-              this.refresh()
-            }
-          })
-          .catch(error => console.log(error))
+        this.saveData();
        }
+      },
+      saveData(){
+        fetch(`${this.api}/api/post/${this.postData._id}`, {
+          method: 'PUT',
+          headers: {
+            'authorization': this.token,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(this.postData)
+        })
+        .then(response => response.json())
+        .then(data => {
+          if(data.error) {
+            console.log(error)
+          } else {
+            this.postData.edit = false;
+            this.refresh()
+          }
+        })
+        .catch(error => console.log(error))
+      },
+      like(){
+       
+        this.postData.usersLiked.push(this.userId);
+        this.postData.likes++;
+        const index = this.postData.usersDisliked.findIndex((d) => d === this.userId);
+        if (index !== -1) {
+          this.postData.usersDisliked.splice(index, 1);
+          this.postData.dislikes--;
+        }
+        this.saveData();
+
+      },
+      dislike(){
+        
+        this.postData.usersDisliked.push(this.userId);
+        this.postData.dislikes++;
+        const index = this.postData.usersLiked.findIndex((d) => d === this.userId);
+        if (index !== -1) {
+          this.postData.usersLiked.splice(index, 1);
+          this.postData.likes--;
+        }
+        this.saveData();
+      },
+      contains: function(data, id){
+        return (data || []).findIndex((d) => (d === id)) !== -1
       }
       
     },
+    
     created: function () {
       this.showOneUser()
-    }
+    },
+   
   }
 </script>
 <template>
   <div class="card mb-3 m-auto d-flex container">
 
     <h5 class="mb-2"><strong>{{user.lastname}} {{user.firstname}}</strong></h5>
-    <div class="card-body container">
+    <div class="card-body container" v-if="!postData.edit">
       <img v-if="postData.image != ''" :src="postData.image" alt="..." class="w-100" />
       <p class="card-text">{{postData.post}}</p>
       <div>
         <div class="d-flex justify-content-between">
           <div class="mb-2 mt-3 d-flex gap-2">
             <div class="d-flex gap-1">
-              <button type="button" class="btn btn-danger  ms-auto" @click=" modifyOnePost()">Modifier</button>
+              <button type="button" class="btn btn-danger  ms-auto" v-if="userId === postData.userId" @click="postData.edit = true">Modifier</button>
             </div>
             <div class="d-flex gap-1">
-              <button type="button" class="btn btn-danger ms-auto"   @click="deleteOnePost()">Supprimer</button>
+              <button type="button" class="btn btn-danger ms-auto"  v-if="userId === postData.userId"  @click="deleteOnePost()">Supprimer</button>
             </div>
           </div>
           <div class="d-flex " >
             <div class=" gap-1">
-              <button type="button" class="btn-like ms-auto">
+              <span>Likes: {{postData.likes}}</span>
+              <button type="button" class="btn-like ms-auto" @click="like()" v-if="!contains(postData.usersLiked, userId)">
                 j'aime
               </button>
+              <span v-if="contains(postData.usersLiked, userId)">You liked!</span>
             </div>
             <div class=" gap-1 ms-4">
               <i class="bi bi-suit-heart"></i>
-              <button type="button" class="btn-like ms-auto ">
+              <span>Dislikes: {{postData.dislikes}}</span>
+              <button type="button" class="btn-like ms-auto " @click="dislike()" v-if="!contains(postData.usersDisliked, userId)">
                j'aime pas
               </button>
+              <span v-if="contains(postData.usersDisliked, userId)">You disliked!</span>
             </div>
           </div>
             
@@ -114,6 +152,12 @@
         </div>
       </div>
     </div>
+    <div class="card-body container" v-if="postData.edit">
+      <textarea v-model="postData.post" class="form-control" placeholder="Ecrivez quelque chose" id="floatingTextarea2" style="height: 100px"></textarea>
+      <button type="button" class="btn btn-danger  ms-auto" @click="modifyOnePost()">Save</button>
+      <button type="button" class="btn btn-danger  ms-auto" @click="postData.edit = false">Cancel</button>
+    </div>
+
   </div>
 </template>
 
